84e77ed7a39e6b1f1619ff3b672525a6da4efa02
VadimVyalov <113896657+VadimVyalov@users.noreply.github.com>
2023-05-17 17:20:11 +0000

    Deploying to gh-pages from @ VadimVyalov/tech-project@19080fdde234ffdf9f4acc697f8b35c0fc4229ea 🚀

---
+++ assets/SharedLayout-b6463633.js
@@ -0,0 +1,5 @@
+import{j as s,N as d}from"./index-30936eb3.js";const c=({children:e})=>s.jsx("div",{className:"container max-w-7xl  my-0 mx-auto py-0 px-4 pb-16",children:e}),b=({children:e})=>s.jsx("header",{className:`flex items-center justify-between
+                 gap-4 px-0 py-4 mb-4 border-b 
+                 border-black border-solid `,children:s.jsxs("nav",{className:"flex gap-4",children:[" ",e," "]})}),x=({to:e,children:o})=>{const r="py-2 px-8 rounded-md hover:bg-sky-400 hover:shadow hover:shadow-sky-500 border  border-stone-400 border-solid ",n=`${r} bg-sky-400  text-white`,a=`${r} bg-sky-200 text-black`;return s.jsx(d,{to:e,className:({isActive:t})=>t?n:a,children:o})},l=({link:e,children:o})=>s.jsx("button",{onClick:()=>e(),className:`bg-sky-200 px-3 rounded-md flex items-center gap-2
+                    border border-stone-400 border-solid 
+                  hover:bg-sky-400 hover:shadow hover:shadow-sky-500`,children:o});export{l as B,c as C,b as H,x as M};
