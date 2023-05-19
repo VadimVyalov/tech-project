d4aeda8257dd940de77506f54bb14db778f478fb
VadimVyalov <113896657+VadimVyalov@users.noreply.github.com>
2023-05-19 19:01:57 +0000

    Deploying to gh-pages from @ VadimVyalov/tech-project@b717e49977f95a7a8116a3ba84e8211862d22bcf 🚀

--- assets/Home-2a5abd2b.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-d0307845.js";const t=()=>e.jsx(e.Fragment,{children:" home page"});export{t as default};
---
+++ assets/Home-8043e97c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d951cfbc.js";const t=()=>e.jsx(e.Fragment,{children:" home page"});export{t as default};
