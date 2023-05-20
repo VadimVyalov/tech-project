c2989677f3178c579b008958ea23d9d800fc75b6
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-05-20 22:20:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tech-project/",
-  plugins: [react(), pluginRewriteAll()],
+   plugins: [react()],
 });
