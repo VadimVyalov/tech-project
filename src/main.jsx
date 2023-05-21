137289ebd9d14832fc2a2dc39c570c1d3d3226ce
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-05-21 18:24:27 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import "./fonts/Montserrat-SemiBold.ttf";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter  basename={'/'}>
+    <BrowserRouter  basename={'/tech-project/'}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
